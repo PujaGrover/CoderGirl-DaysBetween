ba9d78731f07d75d6105e2983380be37258c55b4
Puja Grover <puja_grover@yahoo.com>
2019-03-12 12:31:34 -0500

    Week7HwDaysBetween;Tested;commit;Sync;Push

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,10 +7,24 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
+            Console.WriteLine("Enter the first date:");
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter the second date:");
+            DateTime date2 = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("No of days between the given dates is:{0} ", DaysBetween(date1,date2));
             Console.ReadLine();
         }
-
         // TODO: Create that has a method that counts the number of days between two different dates.
+        public static int DaysBetween(DateTime firstDate, DateTime secondDate)
+        {
+            int result = (firstDate - secondDate).Days;
+
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+            
+            return Math.Abs(result);
+        }
     }
 }
